71493c67bca83d627dede02eb08ee5cab73d5c8e
federicoeparisi <federicoeparisi@hotmail.com>
2022-12-09 23:21:06 -0300

    update

--- styles/app.js
+++
@@ -1,5 +0,0 @@
-const bars = document.getElementById('bars');
-const mobileMenu = document.getElementById('mobileMenu');
-bars.addEventListener('click', function() {
-  mobileMenu.classList.toggle('active')
-})
